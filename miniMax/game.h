ddd0e14a55179379cfc837b85be41e59875c42c6
Huan Wang <huhwang@mines.edu>
2017-02-27 20:05:18 -0700

    build the general game board and game logic

--- miniMax/game.h
+++ miniMax/game.h
@@ -11,22 +11,44 @@ enum game_mode {
 class Game
 {
 public:
+    // constructor
     Game();
+   
+    void start();
 
-    void printGameBoard(ostream & out = cout);
+    // functions related to game config and initialize
+    void init(int argc, char ** argv);
+
+private:
+    int * _gameBoard[6];
+    int _gameData[42];
+    int _currentTurn;
+    int _pieceCount;
+    int _player1Score;
+    int _player2Score;
+    int _depth;
+
+    char * _infilename = NULL;
+    char * _outfilename = NULL;
+    const char * _comOutputFile = "computer.txt";
+    const char * _humOutputFile = "human.txt";
+
+    game_mode _gamemode;
+    bool _comNext;
+
+    void setGameMode(char * mode);
+    void setInputfile(char * infile);
+    void setOutputfile(char * outfile);
+    void setNextPlayer(char * next);
+
+    void printGameBoard();
+    void printGameBoardToFile(const char * outfilename);
+    void printPlayerScore();
     bool playPiece(int column);
     void aiPlay();
+    void humanPlay();
     void countScore();
-    void setGameMode(char * gamemode);
-
-    int * gameBoard[6];
-    long currentTurn;
-    int player1Score;
-    int player2Score;
-    int pieceCount;
-    char * filename;
-
-    game_mode gamemode;
-private:
-    int gameData[42];
+    void startOneMove();
+    void startInteractive();
+    void readInputFile();
 };
